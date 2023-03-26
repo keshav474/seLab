75ee39771005853ab2ca9a749e6f52c1b54dd8aa
Keshav Gupta <84160543+keshav474@users.noreply.github.com>
2023-03-27 03:34:38 +0530

    sel cuc FIRST

---
+++ lab/src/test/java/lab/Demo/StepDef.java
@@ -0,0 +1,81 @@
+package lab.Demo;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDef {
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@When("I complete action")
+	public void i_complete_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@When("some other action")
+	public void some_other_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@When("yet another action")
+	public void yet_another_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@Then("I validate the outcomes")
+	public void i_validate_the_outcomes() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@Then("check more outcomes")
+	public void check_more_outcomes() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@Given("I want to write a step with name1")
+	public void i_want_to_write_a_step_with_name1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@When("I check for the {int} in step")
+	public void i_check_for_the_in_step(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@Then("I verify the success in step")
+	public void i_verify_the_success_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@Given("I want to write a step with name2")
+	public void i_want_to_write_a_step_with_name2() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+	@Then("I verify the Fail in step")
+	public void i_verify_the_fail_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println(" jai ");
+	}
+
+
+}
---
+++ lab/src/test/java/lab/Demo/TestRunner.java
@@ -0,0 +1,18 @@
+package lab.Demo;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/Feature_lab/login.feature",
+		glue = "lab.Demo",
+		plugin = {"pretty", "html:target/HtmlReports", "json:target/JSONReports/reports.json",
+				"junit:target/JUnitReports/report.xml"}
+		)
+
+public class TestRunner extends AbstractTestNGCucumberTests{
+
+}
